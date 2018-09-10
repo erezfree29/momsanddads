02b017a2e300e487097ed537136e67255e49c1cc
erez friemagor <erezfree29@gmail.com>
2018-09-10 18:57:00 +0100

    new parent

--- app/controllers/parents_controller.rb
+++ app/controllers/parents_controller.rb
@@ -7,5 +7,15 @@ class ParentsController < ApplicationController
 
  end
 
+ def new
+   if user_signed_in?
+  @user = current_user
+  @parent = Parent.new()
+  @parent.user_id = @user.id
+  end
+
+
+ end
+
 
 end
