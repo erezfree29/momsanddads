4a783e8a4e57409fdf8c135e2473b3634b5e8025
erez friemagor <erezfree29@gmail.com>
2018-09-30 14:09:40 +0100

    models

--- app/models/parent.rb
+++ app/models/parent.rb
@@ -1,6 +1,6 @@
 class Parent < ApplicationRecord
   belongs_to :user
-  # has_one :sexualorientation
-  # has_one :country
+  has_one :sexualorientation
+  has_one :country
   mount_uploader :photo, PhotoUploader
 end
