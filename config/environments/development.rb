9dc68c027bbaa2b05511adb84a56eb2a72ef02d6
erez friemagor <erezfree29@gmail.com>
2018-10-06 22:07:23 +0100

    password

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,9 +33,12 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
+  #
+  config.action_mailer.perform_deliveries = true
+
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
@@ -62,8 +65,6 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   config.action_mailer.default_url_options =
-      { :host => 'https://momsanddads.herokuapp.com' }  #if it is local then 'localhost:3000'
-
-
+      { :host => 'localhost:3000' }  #if it is local then 'localhost:3000'
 
 end
