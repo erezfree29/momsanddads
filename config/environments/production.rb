0725b9ba30655441ee5861df0ce08dacc238268f
erez friemagor <erezfree29@gmail.com>
2018-11-08 10:49:19 +0000

    url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -106,7 +106,7 @@ config.action_mailer.perform_deliveries = true
 
 
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { :host => 'portal.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'momsanddads.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
       :address              => 'smtp.gmail.com',
