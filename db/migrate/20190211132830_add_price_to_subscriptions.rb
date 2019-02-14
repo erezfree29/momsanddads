8dcc55ba6f4f63103ca5d73d5c37fc7195d1187e
erez friemagor <erezfree29@gmail.com>
2019-02-14 12:24:32 +0000

    navbar

--- db/migrate/20190211132830_add_price_to_subscriptions.rb
+++ db/migrate/20190211132830_add_price_to_subscriptions.rb
@@ -1,7 +1,7 @@
 class AddPriceToSubscriptions < ActiveRecord::Migration[5.2]
   def change
     add_monetize :subscriptions, :price, currency: { present: false }
-    t.monetize :amount, currency: { present: false }
+    s.monetize :amount, currency: { present: false }
 
 
   end
